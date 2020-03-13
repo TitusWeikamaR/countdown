d7d5e820dfd4e747ae52c4affd8bc4a88981a767
Titus Weikama R <titobuffalo@gmail.com>
2020-03-13 10:22:16 +0300

    resolved issues with app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,24 @@
-import { NgModule, Component } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
-import{SpeakersComponent} from './speakers/speakers.component';
-import{NewsComponent} from './news/news.component';
-import{HeaderComponent} from './header/header.component';
-import {FacilatatorsComponent} from './facilatators/facilatators.component';
-import { TicketsComponent } from './tickets/tickets.component';
-import { BulletinComponent } from './bulletin/bulletin.component';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AboutUsComponent } from "./about-us/about-us.component";
+import { SpeakersComponent } from "./speakers/speakers.component";
+import { NewsComponent } from "./news/news.component";
+import { HeaderComponent } from "./header/header.component";
+import { FacilatatorsComponent } from "./facilatators/facilatators.component";
+import { TicketsComponent } from "./tickets/tickets.component";
+import { BulletinComponent } from "./bulletin/bulletin.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {path:'home', component:HeaderComponent},
-  {path:'aboutus', component:AboutUsComponent},
-  {path:'speakers', component:FacilatatorsComponent},
-  {path:'tickets', component:TicketsComponent},
-  {path: 'news', component:BulletinComponent},
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HeaderComponent },
+  { path: "aboutus", component: AboutUsComponent },
+  { path: "speakers", component: FacilatatorsComponent },
+  { path: "tickets", component: TicketsComponent },
+  { path: "news", component: BulletinComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
